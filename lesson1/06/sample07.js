b04403b9cab3c35e349cdc11ec5dcc27290a0581
tomoya1031 <rowingwant@yahoo.co.jp>
2020-12-17 16:03:53 +0900

    1-6

--- lesson1/06/sample07.js
+++ lesson1/06/sample07.js
@@ -1,5 +1,24 @@
 const myFruits4 = ["りんご","みかん","すいか"]
 myFruits4.unshift("いちご")
-console.log(myFruits3)
+console.log(myFruits4)
+
 myFruits4.push("ばなな")
-console.log(myFruits3)
+console.log(myFruits4)
+
+myFruits4.shift()
+console.log(myFruits4)
+
+myFruits4.pop()
+console.log(myFruits4)
+
+myFruits4.splice(1,1)
+console.log(myFruits4)
+
+myFruits4.splice(1,0,"いちご")
+console.log(myFruits4)
+
+myFruits4.reverse()
+console.log(myFruits4)
+
+myFruits4.sort()
+console.log(myFruits4)
---
+++ lesson1/06/sample08.js
@@ -0,0 +1,9 @@
+const myFruits5 = ["りんご","みかん","すいか","めろん","ばなな","もも"]
+const newFruits = myFruits5.slice(3)
+console.log(newFruits)
+
+const newFruits1 = myFruits5.slice(-2)
+console.log(newFruits1)
+
+const newFruits2 = myFruits5.slice(2,4)
+console.log(newFruits2)
