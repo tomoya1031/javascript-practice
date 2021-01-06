354b29c335b9060c671450ee6cdd822d26100110
tomoya1031 <rowingwant@yahoo.co.jp>
2021-01-06 23:32:04 +0900

    2-2

---
+++ lesson3/01/sample01.js
@@ -0,0 +1,29 @@
+let myObject = {}
+
+console.log(myObject)
+
+let myThings = {
+  sports: "サッカー",
+  hobby: "テーブルトークRPG",
+  eat: "カレーライス"
+}
+
+console.log(myThings)
+
+let myThings1 = {
+  sports: "サッカー",
+  hobby: "テーブルトークRPG",
+  eat: "カレーライス"
+}
+
+let myThings2 = {
+  sports1: "サッカー",
+  hobby2: "テーブルトークRPG",
+  food2: "カレーライス"
+}
+
+// ドット表記
+console.log(myThings2.food2) // カレーライスと表示します
+
+// ブラケット表記
+console.log(myThings2["food"]) // カレーライスと表示します
