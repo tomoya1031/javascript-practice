217529fc9e5846f9c62987c6d43516e917328c8d
tomoya1031 <rowingwant@yahoo.co.jp>
2021-01-08 23:58:11 +0900

    2-2

--- lesson3/01/sample01.js
+++ lesson3/01/sample01.js
@@ -10,23 +10,17 @@ let myThings = {
 
 console.log(myThings)
 
-let myThings1 = {
-  sports: "サッカー",
-  hobby: "テーブルトークRPG",
-  eat: "カレーライス"
-}
-
-let myThings2 = {
-  sports1: "サッカー",
-  hobby2: "テーブルトークRPG",
-  food2: "カレーライス"
-}
-
 // ドット表記
-console.log(myThings2.food2) // カレーライスと表示します
+console.log(myThings.food) // カレーライスと表示します
 
 // ブラケット表記
-console.log(myThings2["food"]) // カレーライスと表示します
+console.log(myThings["food"]) // カレーライスと表示します
+
+let div = document.querySelector("#root")
+
+div.innerHTML = "好きなスポーツ：" + myThings.sports + "<br />" +
+                "好きな遊び：" + myThings.hobby + "<br />" +
+                "好きな食べ物：" + myThings.food
 
 let foods = {
   japanese_food: "寿司",
@@ -37,3 +31,5 @@ let suffix = "_food"
 
 console.log(foods["japanese" + suffix]) // 寿司と表示します
 console.log(foods["italian" + suffix])  // ピザと表示します
+
+console.log(foods.mexican_food) // undefinedを表示します
