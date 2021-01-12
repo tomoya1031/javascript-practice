156e4d9f54949caa88dd9a623427e2da7e919bf0
tomoya1031 <rowingwant@yahoo.co.jp>
2021-01-12 23:26:05 +0900

    2-timer

---
+++ lesson2/timer/timer.js
@@ -0,0 +1,33 @@
+let finish
+
+let intervalId
+
+/**
+ * タイマーを開始する
+ */
+function startTimer() {
+  let second = document.querySelector("#time-input").value
+
+  // スタートした時刻とタイマーの時間を足した合計が終了時刻
+  finish = Date.now() + second * 1000
+
+  intervalId = setInterval(checkRemainingTime, 50)
+}
+
+/**
+ * タイマーを終了する
+ */
+function stopTimer() {
+}
+
+/**
+ * 残り時間をチェックする繰り返し
+ */
+function checkRemainingTime() {
+}
+
+/**
+ * 残り時間を表示する
+ */
+function setDisplay(second) {
+}
