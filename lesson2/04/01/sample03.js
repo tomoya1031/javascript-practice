8bc35601e78044d13f9e14fa88696275d2a9d4ce
tomoya1031 <rowingwant@yahoo.co.jp>
2021-01-18 21:42:15 +0900

    2-4

--- lesson2/04/01/sample03.js
+++ lesson2/04/01/sample03.js
@@ -1,4 +1,4 @@
-let students = [
+let students3 = [
   {
     name: "Taro",
     language: 84,
@@ -18,3 +18,25 @@ let students = [
     science: 70
   }
 ]
+
+students3
+  .map(function (student){
+    let total = student.language +
+                student.maths +
+                student.science
+    return {
+      name: student.name,
+      total: total
+    }
+  })
+  .filter(function (student){
+    return student.total >= 190
+  })
+  .forEach(function (student){
+    console.log(
+      student.name +
+      "さんの合計点数は" +
+      student.total +
+      "点です"
+    )
+  })
---
+++ lesson2/04/01/sample04.js
@@ -0,0 +1,21 @@
+let students4 = []
+
+students4.push({
+	name: "Taro",
+	language: 84,
+	maths: 62,
+	science: 50
+})
+
+console.log(students4)
+
+students4 = students4.concat([
+	{
+		name: "kengi",
+		language: 54,
+		maths: 70,
+		science: 62
+	}
+])
+
+console.log(students4)
